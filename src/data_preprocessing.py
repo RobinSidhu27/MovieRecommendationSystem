d065e7a08799e55b1a6e9fd917f02dc879129131
Robin <sidhubrar2774@gmail.com>
2025-11-07 11:49:28 +0530

    Fixed pre processing for render deployment

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,55 +1,77 @@
 import numpy as np
 import pandas as pd
 import ast
+import os
 
 def load_and_clean_data():
-  
-  def convert(obj):
-    L = []
-    for i in ast.literal_eval(obj):
-      L.append(i['name'])
-    return L
 
-  def convert3(obj):
-    L = []
-    counter = 0
-    for i in ast.literal_eval(obj):
-      if counter != 3:
-        L.append(i['name'])
-        counter += 1
-      else:
-        break
-    return L
+    # --------------------------
+    # Helper functions
+    # --------------------------
+    def convert(obj):
+        L = []
+        for i in ast.literal_eval(obj):
+            L.append(i['name'])
+        return L
 
-  def fetch_director(obj):
-    L = []
-    for i in ast.literal_eval(obj):
-      if i['job'] == 'Director':
-        L.append(i['name'])
-        break
-    return L
+    def convert3(obj):
+        L = []
+        counter = 0
+        for i in ast.literal_eval(obj):
+            if counter != 3:
+                L.append(i['name'])
+                counter += 1
+            else:
+                break
+        return L
 
-  movies = pd.read_csv('/Users/robinsidhu/Robin/Projects/Movie_Reco/archive/tmdb_5000_movies.csv')
-  credits = pd.read_csv('/Users/robinsidhu/Robin/Projects/Movie_Reco/archive/tmdb_5000_credits.csv')
+    def fetch_director(obj):
+        for i in ast.literal_eval(obj):
+            if i['job'] == 'Director':
+                return [i['name']]
+        return []
 
-  movies = movies.merge(credits, on='title')
+    # --------------------------
+    # Correct file paths (Render compatible)
+    # --------------------------
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, "..", "archive")
 
-  movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+    movies_path = os.path.join(DATA_DIR, "tmdb_5000_movies.csv")
+    credits_path = os.path.join(DATA_DIR, "tmdb_5000_credits.csv")
 
-  movies['genres'] = movies['genres'].apply(convert)
-  movies['keywords'] = movies['keywords'].apply(convert)
-  movies['cast'] = movies['cast'].apply(convert3)
-  movies['crew'] = movies['crew'].apply(fetch_director)
-  movies['overview'] = movies['overview'].fillna('').apply(lambda x:x.split())
+    movies = pd.read_csv(movies_path)
+    credits = pd.read_csv(credits_path)
 
-  movies['genres'].apply(lambda x:[i.replace(" ", "") for i in x])
-  movies['keywords'].apply(lambda x:[i.replace(" ", "") for i in x])
-  movies['crew'].apply(lambda x:[i.replace(" ", "") for i in x])
-  movies['overview'].apply(lambda x:[i.replace(" ", "") for i in x])
+    # --------------------------
+    # Merge and filter columns
+    # --------------------------
+    movies = movies.merge(credits, on='title')
 
-  movies['tags'] = movies['genres'] + movies['crew'] + movies['overview'] + movies['cast'] + movies['keywords']
+    movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
 
-  newdf = movies[['movie_id', 'title', 'tags']]
-  newdf['tags'] = newdf['tags'].apply(lambda x:" ".join(x))
+    # --------------------------
+    # Apply transformations
+    # --------------------------
+    movies['genres'] = movies['genres'].apply(convert)
+    movies['keywords'] = movies['keywords'].apply(convert)
+    movies['cast'] = movies['cast'].apply(convert3)
+    movies['crew'] = movies['crew'].apply(fetch_director)
+    movies['overview'] = movies['overview'].fillna('').apply(lambda x: x.split())
 
-  return newdf
+    # Actually REPLACE spaces inside items
+    movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ", "") for i in x])
+    movies['keywords'] = movies['keywords'].apply(lambda x: [i.replace(" ", "") for i in x])
+    movies['crew'] = movies['crew'].apply(lambda x: [i.replace(" ", "") for i in x])
+    movies['overview'] = movies['overview'].apply(lambda x: [i.replace(" ", "") for i in x])
+    movies['cast'] = movies['cast'].apply(lambda x: [i.replace(" ", "") for i in x])
+
+    # --------------------------
+    # Build final tags
+    # --------------------------
+    movies['tags'] = movies['genres'] + movies['crew'] + movies['overview'] + movies['cast'] + movies['keywords']
+
+    newdf = movies[['movie_id', 'title', 'tags']].copy()
+    newdf['tags'] = newdf['tags'].apply(lambda x: " ".join(x))
+
+    return newdf
